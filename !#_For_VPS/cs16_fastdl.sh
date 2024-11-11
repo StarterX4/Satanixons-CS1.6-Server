d6ea1eca8d61f3f157f9ac38193b08f0dcf1ca9a
Dominik Adrian Grzywak <starterx4@gmail.com>
2024-11-11 02:05:12 +0100

    Local FastDL script: check if npx (npm) is installed

--- !#_For_VPS/cs16_fastdl.sh
+++ !#_For_VPS/cs16_fastdl.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-#BINDIR=$(dirname "$(readlink -fn "$0")")
-#cd "$BINDIR/cs16"
-screen -L -Logfile cs16/screenlog_FastDL.log -d -m -S CS16_FastDL npx http-server cs16/cstrike_downloads/ -p 2137
+DIR=cs16
+# Check if npx (npm) is installed
+if ! command -v npx &>/dev/null;
+then
+    echo "Error: npm/npx is not installed."
+    exit 1
+fi
+screen -L -Logfile $DIR/screenlog_FastDL.log -d -m -S CS16_FastDL npx -y http-server $DIR/cstrike_downloads/ -p 2137
